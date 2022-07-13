60e3f7f499b51d7a7f12861a6011d270d28555f5
Raytene Han <raytenehan@gmail.com>
2022-07-13 16:34:19 -0700

    created findSpotForCol function

--- connect4.js
+++ connect4.js
@@ -69,8 +69,14 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return bottom empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 5
-  return 5;
+  // iterate over rows y and check if board[y][x] is null. return y at the first null
+  for (let y = HEIGHT - 1; y >= 0 ; y--) {
+    if (!board[y][x]) {
+      return y;
+    }
+  }
+
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
